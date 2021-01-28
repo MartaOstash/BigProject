61b069856a729962132f2f1dbe055ecbbf3c5357
MartaOstash <martaostash.18@gmail.com>
2021-01-27 21:41:39 -0500

    planet 1 is ready to merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,14 @@ public class Main {
         FactorX.Sun=1;
         FactorX.Moon=2;
         FactorX.Planets=2;
-
         SUN troyo=new SUN();
         troyo.Heat="5787kj";
         troyo.temperature="65348k";
         troyo.Radius="998877899km";
+
+        Planet1 Fearo =new Planet1();
+        Fearo.radius="76543km";
+        Fearo.color="red";
 
     }
 }
---
+++ src/com/company/Planet1.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Planet1 {
+    String radius;
+    String color;
+
+}
+
